112dc7ba76d90c5705d494a7fb732ff3085d0ed5
yamauchi.ta <yamauchi.ta@mythos-jp.com>
2019-11-26 21:58:21 +0900

    ・スクリプト実行を可能にしました ・ルートを変更しました。

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
 from bottle import route, run, default_app, template
 
-@route('/test')
-def test():
-    return 'test success'
+@route('/battler')
+def battler():
+    return 'battler success!!'
 
-@route('/test/sample')
-def test():
+@route('/battler/sample')
+def sample():
     title='Hello world'
     return template('sample', title=title)
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=3031, debug=True, reloader=True)
+    run(host='0.0.0.0', port=8000, debug=True, reloader=True)
 else:
     application = default_app()
