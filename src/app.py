7122d7f252af77ef05b2488e6f0ec3ef90482d4c
yamauchi.ta <yamauchi.ta@mythos-jp.com>
2019-11-30 20:49:02 +0900

    モデルを使うように変更しました。

--- src/app.py
+++ src/app.py
@@ -6,15 +6,23 @@ main: Bottle = Bottle()
 
 @main.route('/static/<filePath:path>')
 def static(filePath):
+    """
+    Staticファイルのパス設定
+    """
     return static_file(filePath, root='./static')
 
+@main.route('/battler')
+def battler():
+    return 'battler success!!'
+
+
+# 以下、URLに対応する別々のモジュールを呼ぶ
 main.mount('/battler/sample', sample.app)
 
-# @route('/battler')
-# def battler():
-#     return 'battler success!!'
 
 if __name__ == '__main__':
+    # 開発用アプリ起動
     main.run(host='0.0.0.0', port=8000, debug=True, reloader=True)
 else:
+    # Nginx経由でアプリ起動
     application = main
