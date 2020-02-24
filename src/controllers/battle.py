37f4680610cf7e3f38e06e4bbd839b840f87d2cd
Smith11764 <fuji.feather10@gmail.com>
2020-02-25 00:37:10 +0900

    キャラクター選択のラフ完成
    キャラクター選択のラフ完成

--- src/controllers/battle.py
+++ src/controllers/battle.py
@@ -9,10 +9,58 @@ app: Bottle = Bottle()
 @app.route('/')
 def index():
     """
-    モンスター生成画面表示
+    対戦前ページ
 
     Returns
     ----------
     templateオブジェクト
     """
-    return template('vs')
+
+    teams = redismodel.RedisTeams().select()
+    return template('vs', teams = teams)
+
+# @app.route('/choose')
+# def choose():
+#     """
+#     対戦キャラ選択ページ
+
+#     Returns
+#     ----------
+#     templateオブジェクト
+#     """
+#     team = 'A'
+#     monsters = redismodel.RedisMonster().select_all(team)
+
+#     return template('choose', monsters=monsters)
+
+@app.route('/', 'post')
+def post():
+    """
+    対戦キャラ選択ページ
+
+    Returns
+    ----------
+    templateオブジェクト
+    """
+
+    # ajaxで渡ってきたteamを受け取り表示に渡す
+    team = request.forms.getunicode('team')
+    logging.info(team)
+    monsters = redismodel.RedisMonster().select_all(team)
+
+    return template('choose', monsters=monsters)
+
+@app.route('/register', 'post')
+def register():
+    """
+    バトルに使うキャラの決定
+    """
+    id = request.forms.getunicode('id')
+    # logging.info(target)
+    monster = redismodel.RedisMonster().select(id)
+    monster = {
+        'team' : monster.get_team()
+        , 'name' : monster.get_name()
+    }
+
+    return monster
--- src/static/js/vs.js
+++ src/static/js/vs.js
@@ -1,3 +1,64 @@
+// bgmの再生
 $(document).ready( function(){
     document.getElementById('bgm').play();
 });
+
+// プレイヤー選択ボタン押下時にチーム選択をさせる
+$(function () {
+    $('.choose_button').on('click', function () {
+        $(this).hide();
+        $(this).next('.choose_team').show();
+        $(this).parent().find('.chosen_monster').attr('id', 'target')
+    });
+});
+
+// キャラ選択モーダル
+$(document).ready(function() {
+    // チーム選択ボタン押下でキャラ選択画面に移る
+    $('.modaal').modaal({
+        before_open: function() {
+            $.ajax({
+                type: 'post',
+                url: 'battle',
+                data: {
+                    team: $('#target').parent().prev('.choose_team').find('.team option:selected').data("team"),
+                },
+            })
+            .done(function(response) {
+                $('#choice').html(response);
+            })
+        },
+        content_source: '#modal',
+        // is_locked: true,
+        // hide_close: true
+    });
+
+    // キャラ決定
+    $('#choice').on('click', '.decide-character', function(){
+        $.ajax({
+            type: 'POST',
+            url: 'battle/register',
+            data: {
+                id: $(this).data("key"),
+            },
+        })
+        .done(function(response) {
+            $('.modaal').modaal('close');
+            $('#target').html(response['team'] + '-' + response['name']);
+            $('#target').parent().prev('.choose_team').find('.team option:selected', false);
+            $('#target').removeAttr('id');
+        })
+        .fail(function(response) {
+            console.log('fail');
+        })
+    });
+
+    // バトルページへhiddenでモンスター情報を渡す
+    $('.wrapper').on('click', '#go-battle', function(){
+        var first_character = $('.first-character').find('.chosen_monster').html();
+        $('#player1').val(first_character);
+        var second_character = $('.second-character').find('.chosen_monster').html();
+        $('#player2').val(second_character);
+    });
+
+})
